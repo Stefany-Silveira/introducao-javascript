48e3ebc6ed41f33ee2083f5656910f0d484f5b79
Stefany Silveira <stefany.silveira21@gmail.com>
2022-04-22 17:24:22 -0300

    alteracao @principal.js

--- js/principal.js
+++ js/principal.js
@@ -7,7 +7,7 @@ var pacientes = document.querySelectorAll(".paciente");
 for(var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
 
-    var tdPeso = pacienre.querySelector(".info-peso");
+    var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
 
     var tdAltura = paciente.querySelector(".info-altura");
@@ -34,5 +34,42 @@ for(var i = 0; i < pacientes.length; i++) {
     if(alturaValida && pesoValido) {
         var imc = peso / (altura * altura); 
         tdImc.textContent = imc.toFixed(2);
+    }   
 }
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
+    titulo.addEventListener("click", function() {
+        console.log("fui clicado.")
+    });

71ee45484066fe95276db0c7975502ff473a04b8
Stefany Silveira <stefany.silveira21@gmail.com>
2022-04-24 00:04:08 -0300

    alteracao @calcula-imc.js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,8 +3,8 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
+for (var i = 0; i < pacientes.length; i++) {
 
-for(var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
 
     var tdPeso = paciente.querySelector(".info-peso");
@@ -15,33 +15,32 @@ for(var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoValido = true;
-    var alturaValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
     if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido!");
-        pesoValido = false;
+        pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
         paciente.classList.add("paciente-invalido");
-}
-    if(altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida");
-        alturaValida = false;
-        tdImc.textContent = "Altura inválida!"
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
         paciente.classList.add("paciente-invalido");
-}
+    }
 
-    if(alturaValida && pesoValido) {
-        var imc = calculaImc(peso, altura); 
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
-    }   
+    }
 }
 
-function calculaImc(peso,altura) {
+function calculaImc(peso, altura) {
     var imc = 0;
-
     imc = peso / (altura * altura);
+
     return imc.toFixed(2);
 }
-
-

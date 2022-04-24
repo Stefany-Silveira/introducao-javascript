b9561365d17861992256d94f743ba438d2828ded
Stefany Silveira <stefany.silveira21@gmail.com>
2022-04-24 15:25:18 -0300

    alteracao @remover-pacientes.js

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,6 +1,6 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-var tabela = document.querySelector("table");
+var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event) {
     event.target.parentNode.classList.add("fadeOut");
@@ -8,11 +8,5 @@ tabela.addEventListener("dblclick", function(event) {
     setTimeout(function() {
         event.target.parentNode.remove();
     }, 500);
-});
 
-/*pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function() {
-        console.log("Fui clicado com um duplo click");
-        this.remove();
-    });
-}); */
+});
